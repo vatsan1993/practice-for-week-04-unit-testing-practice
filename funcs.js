03b57599869bb95de2bbfa5886c00fcf2f3ca1c1
Srivatsan <vatsan1993@gmail.com>
2023-07-16 16:07:10 +0530

    finished part 1

--- funcs.js
+++ funcs.js
@@ -1,14 +1,23 @@
 function isFive(num) {
   // Your code here
+  return num == 5;
 }
 
 function isOdd(number) {
   // Your code here
+  if (!(typeof number === 'number')) {
+    throw new Error('Unsupported data');
+  }
+  return number % 2 === 1 || number % 2 === -1;
 }
 
 function myRange(min, max, step = 1) {
   // Your code here
+  const newArr = [];
+  for (; min <= max; min += step) {
+    newArr.push(min);
+  }
+  return newArr;
 }
-
 
 module.exports = { isFive, isOdd, myRange };
